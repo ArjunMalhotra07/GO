5eae3539cc845fa9f6831ee97d55d31e955571e9
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-18 02:03:11 +0530

    Closest Strings in Array

---
+++ Strings/ClosestStrings.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	s := []string{"geeks", "for", "geeks", "contribute", "practice"}
+	fmt.Println(s)
+	min(s, "geeks", "practice")
+
+}
+
+func min(s []string, word1 string, word2 string) {
+	p := fmt.Println
+	arr2 := []int{}
+	arr1 := []int{}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == word2 {
+			arr2 = append(arr2, i)
+		}
+	}
+	for i := 0; i < len(s); i++ {
+		if s[i] == word1 {
+			arr1 = append(arr1, i)
+		}
+	}
+	p(arr2)
+	p(arr1)
+
+	minDistance(arr1, arr2)
+
+}
+
+func minDistance(arr1 []int, arr2 []int) {
+	minDist := []int{}
+
+	for i := 0; i < len(arr1); i++ {
+		for j := 0; j < len(arr2); j++ {
+
+			testDist := arr1[i] - arr2[j]
+
+			if testDist < 0 {
+				testDist *= -1
+			}
+			minDist = append(minDist, testDist)
+
+		}
+	}
+
+	fmt.Println(minDist)
+	sort.Ints(minDist)
+	fmt.Println(minDist[0])
+
+}
