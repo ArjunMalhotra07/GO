4eb7cbc524f590b6e0c4213532099a801882ec67
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-18 02:03:43 +0530

    Big Integer GoLang

---
+++ Strings/BigInteger.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+
+	var smallnum, _ = new(big.Int).SetString("2188824200011112223", 10)
+	num := smallnum.Uint64()
+	fmt.Println(check(num))
+}
+
+func check(num uint64) uint64 {
+	if num/7 != 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
