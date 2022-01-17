e194f20acba86b0e6f9eea7c7ab01097421b00ba
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-18 00:43:46 +0530

    String Builder

--- Strings/stringBuilder.go
+++ Strings/stringBuilder.go
@@ -9,18 +9,13 @@ func main() {
 
 	var sb strings.Builder
 	fmt.Println("This is a string builder", sb.String())
-	fmt.Println(sb.Cap())
-	fmt.Println(sb.Len())
+
 	sb.WriteString("Hello")
 	fmt.Println("This is a string builder:", sb.String())
-	fmt.Println(sb.Cap())
-	fmt.Println(sb.Len())
+
 	sb.WriteString(" World")
 	fmt.Println("This is a string builder:", sb.String())
-	sb.Grow(100)
-	fmt.Println(sb.Cap())
-	fmt.Println(sb.Len())
-	sb.WriteByte(65)
+	sb.WriteString(" Earth")
 	fmt.Println("This is a string builder:", sb.String())
 
 }
