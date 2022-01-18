f88412706609f852576621f7de131510e4127ae1
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-18 16:15:38 +0530

    Reverse string using recursion

---
+++ Strings/04reverseRecursion.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	test := "Arjun"
+	ans := reverse(test, len(test)-1)
+	fmt.Println(ans)
+
+}
+
+func reverse(s string, length int) string {
+	ans := length
+
+	byte_String := []byte(s)
+	ans_String := []byte{}
+
+	for ans >= 0 {
+		ans_String = append(ans_String, byte_String[ans])
+		return string(ans_String) + reverse(s, ans-1)
+	}
+
+	return ""
+}
