7b878ff44ac840f1ce7b8c5e48ab22dbe6d125c9
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-01-19 19:24:48 +0530

    Shiffle String

---
+++ Strings/07shuffleString.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	p := fmt.Println
+	indices := []int{4, 5, 6, 7, 0, 2, 1, 3}
+	p(restoreString("codeleet", indices))
+}
+
+func restoreString(s string, indices []int) string {
+	ans := make([]byte, len(s))
+	for i, index := range indices {
+		ans[index] = s[i]
+	}
+	return string(ans)
+}
