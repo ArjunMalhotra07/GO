52a6e1632709025ad707fb774f441acd6a4a7c95
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-04-08 20:34:54 +0530

    Duplicate characters in a string

--- Strings/03duplicateBytes.go
+++ Strings/03duplicateBytes.go
@@ -3,29 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	duplicate("sunaina")
+	fmt.Println("Enter Word to find Duplicate Characters -- ")
+	var input string
+	fmt.Scanln(&input)
+	duplicate1(input)
 }
 
-func duplicate(s string) {
+func duplicate1(s string) {
 	p := fmt.Print
 	p1 := fmt.Println
 	test := []byte(s)
 	ansArray := []byte{}
 
 	for _, ans := range test {
-		p(string(ans))
-		p1()
+		p(string(ans), " ")
+
 	}
 	p1()
 
 	for i := 0; i < len(test)-1; i++ {
-		//count := 0
 		for j := i + 1; j < len(test); j++ {
-			if string(test[i]) == string(test[j]) {
+			if (test[i]) == (test[j]) {
 				ansArray = append(ansArray, test[i])
 			}
 		}
 
 	}
-	p1(string(ansArray))
+	if len(ansArray) == 0 {
+		p1("No Duplicate Characters")
+	} else {
+		p1("Duplicate Characters -- ", string(ansArray))
+	}
+
 }
