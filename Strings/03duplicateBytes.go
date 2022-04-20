31e0a8ac4606d22ebeb044d0842159c1b5dcb7c4
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-04-20 20:10:44 +0530

    Count Letters in a String

--- Strings/03duplicateBytes.go
+++ Strings/03duplicateBytes.go
@@ -7,38 +7,10 @@ func main() {
 	f("Enter Word to find Duplicate Characters -- ")
 	var input string
 	fmt.Scanln(&input)
-	duplicate1(input)
+
 	testMap := findDuplicates(input)
 	f(testMap)
 	f(countDuplicates(testMap))
-}
-
-func duplicate1(s string) {
-	p := fmt.Print
-	p1 := fmt.Println
-	test := []byte(s)
-	ansArray := []byte{}
-
-	for _, ans := range test {
-		p(string(ans), " ")
-
-	}
-	p1()
-
-	for i := 0; i < len(test)-1; i++ {
-		for j := i + 1; j < len(test); j++ {
-			if (test[i]) == (test[j]) {
-				ansArray = append(ansArray, test[i])
-			}
-		}
-
-	}
-	if len(ansArray) == 0 {
-		p1("No Duplicate Characters")
-	} else {
-		p1("Duplicate Characters -- ", string(ansArray))
-	}
-
 }
 
 func findDuplicates(s string) map[string]int {
