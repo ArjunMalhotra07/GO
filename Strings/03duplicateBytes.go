ab9b91141dada76eebcb50483ff252a666c9dd4a
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-04-20 20:09:05 +0530

    Count Letters in a String

--- Strings/03duplicateBytes.go
+++ Strings/03duplicateBytes.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Enter Word to find Duplicate Characters -- ")
+	f := fmt.Println
+	f("Enter Word to find Duplicate Characters -- ")
 	var input string
 	fmt.Scanln(&input)
 	duplicate1(input)
+	testMap := findDuplicates(input)
+	f(testMap)
+	f(countDuplicates(testMap))
 }
 
 func duplicate1(s string) {
@@ -35,4 +39,39 @@ func duplicate1(s string) {
 		p1("Duplicate Characters -- ", string(ansArray))
 	}
 
+}
+
+func findDuplicates(s string) map[string]int {
+	countMap := make(map[string]int)
+	count := 0
+	testString := []byte(s)
+	length := len(s) - 1
+	for i := 0; i < len(s)-1; i++ {
+		if _, found := countMap[string(testString[i])]; !found {
+			for j := i + 1; j < len(s); j++ {
+				if testString[i] == testString[j] {
+					count += 1
+				}
+			}
+			countMap[string(testString[i])] = count + 1
+			count = 0
+		}
+
+	}
+	if _, found := countMap[string(testString[length])]; !found {
+		countMap[string(testString[length])] = 1
+	}
+	return countMap
+}
+
+func countDuplicates(testMap map[string]int) []string {
+	ansArray := []string{}
+
+	for letter, count := range testMap {
+		if count == 2 {
+			ansArray = append(ansArray, letter)
+		}
+	}
+
+	return ansArray
 }
