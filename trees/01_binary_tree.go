4f564508fd0d1bc7f0affa469db62a73affa7fe4
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2024-02-21 14:30:16 +0530

    added height and balance factor function in trees

--- trees/01_binary_tree.go
+++ trees/01_binary_tree.go
@@ -1,12 +1,16 @@
 package trees
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // ! Node
 type Node struct {
-	Key   int
-	Left  *Node
-	Right *Node
+	Key    int
+	Left   *Node
+	Right  *Node
+	Height float64
 }
 
 // ! Insert
@@ -26,6 +30,23 @@ func (t *Node) InsertIntotree(value int) {
 			t.Right.InsertIntotree(value)
 		}
 	}
+	t.Height = math.Max(height(t.Left), height(t.Right)) + 1
+	balanceFactor := getBalanceFactor(t)
+	fmt.Println("Balance Factor", balanceFactor, " Key", t.Key)
+}
+func getBalanceFactor(t *Node) float64 {
+	if t == nil {
+		return 0
+	} else {
+		return height(t.Left) - height(t.Right)
+	}
+}
+func height(t *Node) float64 {
+	if t == nil {
+		return 0
+	} else {
+		return t.Height
+	}
 }
 
 // ! Search
@@ -43,9 +64,9 @@ func (t *Node) SearchValueInTree(n int) bool {
 }
 
 // ! Print Tree
-func (t *Node) PreOrderTraversal(ans []int) []int {
+func (t *Node) PreOrderTraversal(ans []*Node) []*Node {
 	if t != nil {
-		ans = append(ans, t.Key)
+		ans = append(ans, t)
 	}
 	if t.Left != nil {
 		ans = t.Left.PreOrderTraversal(ans)
@@ -55,7 +76,7 @@ func (t *Node) PreOrderTraversal(ans []int) []int {
 	}
 	return ans
 }
-func (t *Node) PostOrderTraversal(ans []int) []int {
+func (t *Node) PostOrderTraversal(ans []*Node) []*Node {
 	if t.Left != nil {
 		ans = t.Left.PostOrderTraversal(ans)
 	}
@@ -63,17 +84,17 @@ func (t *Node) PostOrderTraversal(ans []int) []int {
 		ans = t.Right.PostOrderTraversal(ans)
 	}
 	if t != nil {
-		ans = append(ans, t.Key)
+		ans = append(ans, t)
 	}
 	return ans
 }
-func (t *Node) InOrderTraversal(ans []int) []int {
+func (t *Node) InOrderTraversal(ans []*Node) []*Node {
 
 	if t.Left != nil {
 		ans = t.Left.InOrderTraversal(ans)
 	}
 	if t != nil {
-		ans = append(ans, t.Key)
+		ans = append(ans, t)
 	}
 	if t.Right != nil {
 		ans = t.Right.InOrderTraversal(ans)
@@ -82,17 +103,29 @@ func (t *Node) InOrderTraversal(ans []int) []int {
 }
 
 func DoTreeOperations() {
-	myTree := &Node{Key: 100}
-	myTree.InsertIntotree(50)
-	myTree.InsertIntotree(150)
-	myTree.InsertIntotree(140)
-	myTree.InsertIntotree(160)
-	myTree.InsertIntotree(40)
-	myTree.InsertIntotree(60)
 
-	fmt.Println(myTree.PreOrderTraversal([]int{}))
-	fmt.Println(myTree.InOrderTraversal([]int{}))
-	fmt.Println(myTree.PostOrderTraversal([]int{}))
+	myTree := &Node{Key: 3}
+	arrayInput(myTree)
+	fmt.Println("Preorder")
+	printAns(myTree.PreOrderTraversal([]*Node{}))
+	fmt.Println("Inorder")
+	printAns(myTree.InOrderTraversal([]*Node{}))
+	fmt.Println("Postorder")
+	printAns(myTree.PostOrderTraversal([]*Node{}))
 	fmt.Println(myTree.SearchValueInTree(160))
 
+}
+
+func arrayInput(treeNode *Node) {
+	arr := []int{2, 1}
+	for i := 0; i < len(arr); i++ {
+		treeNode.InsertIntotree(arr[i])
+	}
+}
+
+func printAns(incomingArray []*Node) {
+	for i := 0; i < len(incomingArray); i++ {
+		fmt.Print("Key ", incomingArray[i].Key, " Height ", incomingArray[i].Height)
+		fmt.Println()
+	}
 }
