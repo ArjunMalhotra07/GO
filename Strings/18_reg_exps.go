2c08a0236b00fa1c29a368d0b78a5284ac35930b
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2024-10-30 11:27:29 +0530

    reg exes

--- Strings/18_reg_exps.go
+++ Strings/18_reg_exps.go
@@ -6,12 +6,81 @@ import (
 )
 
 func DoRegularExpressions18() {
-	regexp1()
+	regexp6()
 }
 func regexp1() {
-	pattern := `^(a|b)+(a|b)$`
-	var re *regexp.Regexp = regexp.MustCompile(pattern)
-	var doesMatch bool = re.MatchString("baba")
-	fmt.Println(doesMatch)
 
+	const pattern string = `^[a-z]{1,}$`
+	check := []string{"abcAda", "", "123", "abcdea", "a", "z", "Aba"}
+	var re *regexp.Regexp = regexp.MustCompile(pattern)
+	fmt.Println("All lowercase with same start and end characters")
+	for _, stringToCheck := range check {
+		var doesMatch bool = re.MatchString(stringToCheck)
+		if !doesMatch {
+			fmt.Printf("%s %v \n", stringToCheck, doesMatch)
+			continue
+		}
+		doesMatch = stringToCheck[0] == stringToCheck[len(stringToCheck)-1]
+		fmt.Printf("%s %v \n", stringToCheck, doesMatch)
+	}
+}
+
+func regexp2() {
+	const pattern string = `^a[a-z]*`
+	check := []string{"abcAda", "", "123", "abcdea", "a", "z", "applesauce123"}
+	var re *regexp.Regexp = regexp.MustCompile(pattern)
+	fmt.Println("Start with 'a' and are followed by zero or more lowercase letters")
+	for _, stringToCheck := range check {
+		var doesMatch bool = re.MatchString(stringToCheck)
+		fmt.Printf("%s %v \n", stringToCheck, doesMatch)
+	}
+}
+func regexp3() {
+	const pattern string = `^[a-z]{3}$`
+	check := []string{"abcAda", "", "123", "abc", "a", "z", "Abc"}
+	var re *regexp.Regexp = regexp.MustCompile(pattern)
+	fmt.Println("Only 3 lowercase")
+	for _, stringToCheck := range check {
+		var doesMatch bool = re.MatchString(stringToCheck)
+		fmt.Printf("%s %v \n", stringToCheck, doesMatch)
+	}
+}
+
+func regexp4() {
+	const pattern string = `^[A-Z]{1,5}$`
+	check := []string{"ABC", "", "123", "AbD", "ABCDE", "ABCDEF", "Abc"}
+	var re *regexp.Regexp = regexp.MustCompile(pattern)
+	fmt.Println("strings that contain only uppercase letters (A-Z) and are between 1 and 5 characters long")
+	for _, stringToCheck := range check {
+		var doesMatch bool = re.MatchString(stringToCheck)
+		fmt.Printf("%s %v \n", stringToCheck, doesMatch)
+	}
+}
+
+func regexp5() {
+	const pattern string = `^s.*5$`
+	check := []string{"s45", "", "5", "s34@905", "s_5", "sBCDEF", "6b5"}
+	var re *regexp.Regexp = regexp.MustCompile(pattern)
+	fmt.Println("start with the letter 's' (case-sensitive), followed by any number of characters (including none), and end with the digit '5'")
+	for _, stringToCheck := range check {
+		var doesMatch bool = re.MatchString(stringToCheck)
+		fmt.Printf("%s %v \n", stringToCheck, doesMatch)
+	}
+}
+
+func regexp6() {
+
+	const pattern string = `^[a-z]{1,}$`
+	check := []string{"abcAda", "", "123", "abcdea", "a", "z", "Aba"}
+	var re *regexp.Regexp = regexp.MustCompile(pattern)
+	fmt.Println("All lowercase with same start and end characters")
+	for _, stringToCheck := range check {
+		var doesMatch bool = re.MatchString(stringToCheck)
+		if !doesMatch {
+			fmt.Printf("%s %v \n", stringToCheck, doesMatch)
+			continue
+		}
+		doesMatch = stringToCheck[0] == stringToCheck[len(stringToCheck)-1]
+		fmt.Printf("%s %v \n", stringToCheck, doesMatch)
+	}
 }
