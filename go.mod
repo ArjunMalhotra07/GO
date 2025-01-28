d2b886667ea0efe85af1aace1c26c1a2849b3501
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2025-01-28 09:36:33 +0530

    added files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ArjunMalhotra07/GO
 
 go 1.23.4
+
+require github.com/go-playground/assert v1.2.1
---
+++ takeu_forward/jump_test.go
@@ -0,0 +1,41 @@
+package takeuforward
+
+import (
+	"testing"
+
+	"github.com/go-playground/assert"
+)
+
+func TestJumpGame(t *testing.T) {
+	tests := []struct {
+		name     string
+		arr      []int
+		expected bool
+	}{
+		{name: "ShouldFail", arr: []int{1, 2, 3, 1, 1, 0, 2, 5}, expected: false},
+		{name: "ShouldFail", arr: []int{3, 2, 1, 0, 4}, expected: false},
+		{name: "ShouldPass", arr: []int{2, 3, 1, 0, 4}, expected: true},
+		{name: "ShouldPass ", arr: []int{1, 2, 4, 1, 1, 0, 2, 5}, expected: true}}
+	for _, testCase := range tests {
+		t.Run("", func(t *testing.T) {
+			actualResult := JumpGame(testCase.arr)
+			assert.Equal(t, actualResult, testCase.expected)
+		})
+	}
+}
+
+func JumpGame(steps []int) bool {
+	max := 0
+	for i := 0; i < len(steps); i++ {
+		if i > max {
+			return false
+		}
+		if steps[i]+i >= len(steps) {
+			return true
+		}
+		if steps[i]+i > max {
+			max = steps[i] + i
+		}
+	}
+	return true
+}
