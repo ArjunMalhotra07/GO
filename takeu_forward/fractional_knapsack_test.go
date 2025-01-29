6b2ec93ac503382e590267aad02e4360dc272c47
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2025-01-29 21:44:57 +0530

    made some changes

--- takeu_forward/fractional_knapsack_test.go
+++ takeu_forward/fractional_knapsack_test.go
@@ -1,7 +1,6 @@
 package takeuforward
 
 import (
-	"fmt"
 	"sort"
 	"testing"
 
@@ -15,21 +14,11 @@ func TestFractionalKnapsack(t *testing.T) {
 		expected int
 	}{
 		{arr: [][]int{{100, 20}, {60, 10}, {100, 50}, {200, 50}}, weight: 90, expected: 380},
-		// Case with exact weight match
-		{arr: [][]int{{100, 20}, {60, 10}, {120, 30}}, weight: 60, expected: 280}, // Takes all items
-		// Case where knapsack weight is 0
-		{arr: [][]int{{100, 20}, {200, 30}}, weight: 0, expected: 0}, // Cannot pick anything
-
-		// Case where all items have the same value-to-weight ratio
-		{arr: [][]int{{50, 10}, {100, 20}, {150, 30}}, weight: 50, expected: 250}, // Takes all
-
-		// Case with only one item
-		{arr: [][]int{{300, 50}}, weight: 30, expected: 180}, // Takes part of the single item
-
-		// Case where all items fit perfectly
-		{arr: [][]int{{100, 10}, {200, 20}, {300, 30}}, weight: 60, expected: 600}, // All items fit exactly
-
-		// Edge case: Empty array
+		{arr: [][]int{{100, 20}, {60, 10}, {120, 30}}, weight: 60, expected: 280},
+		{arr: [][]int{{100, 20}, {200, 30}}, weight: 0, expected: 0},
+		{arr: [][]int{{50, 10}, {100, 20}, {150, 30}}, weight: 50, expected: 250},
+		{arr: [][]int{{300, 50}}, weight: 30, expected: 180},
+		{arr: [][]int{{100, 10}, {200, 20}, {300, 30}}, weight: 60, expected: 600},
 		{arr: [][]int{}, weight: 50, expected: 0},
 	}
 	for _, testCase := range tests {
@@ -42,25 +31,21 @@ func TestFractionalKnapsack(t *testing.T) {
 
 func fractionalKnapsack(arr [][]int, wt int) int {
 	sort.Slice(arr, func(i, j int) bool {
-		return float64(arr[i][0])/float64(arr[i][1]) > float64(arr[j][0])/float64(arr[j][1])
+		return arr[i][0]*arr[j][1] > arr[j][0]*arr[i][1]
 	})
-	fmt.Println(arr)
 	total := 0
 	for i := 0; i < len(arr); i++ {
 		temp := arr[i]
 		if wt <= 0 {
-			return total
+			break
 		}
-		weightToAdd := 0
-
-		if wt-temp[1] >= 0 {
-			weightToAdd = temp[0]
+		if wt >= temp[1] {
+			total += temp[0]
 			wt -= temp[1]
 		} else {
-			weightToAdd = int(wt*temp[0]) / temp[1]
+			total += wt * temp[0] / temp[1]
 			wt = 0
 		}
-		total += weightToAdd
 	}
 	return total
 }
